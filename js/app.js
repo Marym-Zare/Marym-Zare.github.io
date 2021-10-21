246d7d846d717b3c7a73eef7cb3a5dedd91e4660
shahamiri-alireza <shahamiri.alireza81@gmail.com>
2021-10-21 15:58:19 +0330

    added dropdown and modal

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const modal_overlay = document.querySelector('.modal-overlay')
+function toggleModal() {
+  modal_overlay.classList.toggle('active')
+}
+const dropItems = document.querySelector('.drop-items')
+function toggleDrop() {
+  dropItems.classList.toggle('active')
+}
